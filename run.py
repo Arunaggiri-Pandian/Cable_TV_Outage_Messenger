d0950f6ec0dde879ab53b227103a74c782403eaf
Arunaggiri-Pandian <arunaggiri.pandian@gmail.com>
2025-10-15 04:11:37 +0000

    feat: Bind app to 0.0.0.0 for deployment

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     load_dotenv()  # load .env at startup
     app = create_app()
     port = int(os.getenv("PORT", "5001"))
-    app.run(host="127.0.0.1", port=port, debug=True)
+    # app.run(host="127.0.0.1", port=port, debug=True)
+    app.run(host='0.0.0.0', port=8501, debug=True)
